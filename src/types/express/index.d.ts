70b149ba37160264da0158fcbf2a33fa27a394ea
AnmolSingh <devanmolsinghchehal@gmail.com>
2025-07-13 17:33:30 +0530

    did minor changes in index.d.ts

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,19 +1,11 @@
-// import { accessTokenPayloadType } from "../common.type"
+import { accessTokenPayloadType } from "../common.type"
 
-// declare global{
-//     namespace Express{
-//         export interface Request{
-//             user: accessTokenPayloadType,
-//         }
-//     }
-// }
-
-// export {};
-
-import { accessTokenPayloadType } from "../types/common.type";
-
-declare module "express-serve-static-core" {
-  interface Request {
-    user: accessTokenPayloadType;
-  }
+declare global{
+    namespace Express{
+        export interface Request{
+            user: accessTokenPayloadType,
+        }
+    }
 }
+
+export {};
